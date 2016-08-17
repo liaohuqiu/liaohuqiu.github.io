58703763e5640e57250c1a1f426d2511758be383
liaohuqiu <liaohuqiu@gmail.com>
2016-08-17 10:48:16 +0800

    sh _tools/update.sh add one more parameter to enable remove all assert stuff

--- _tools/update.sh
+++ _tools/update.sh
@@ -6,7 +6,7 @@ function exe_cmd()
 }
 function show_help() 
 {
-    echo "usage: sh _tools/$0; please run this script in the project root directory"
+    echo "usage: sh _tools/$0 [all]; please run this script in the project root directory"
     exit 1
 }
 
@@ -15,14 +15,21 @@ if [[ $root_dir == *"/_tools"* ]]; then
     show_help
 fi
 
+build_fe=false
+if [ $# == 1 ]; then
+    build_fe=true
+fi
+
 dest_dir=`readlink -f ../liaohuqiu-master`
 if [ ! -d $dest_dir ];then
     echo "Destination directory is not existent: " $dest_dir
     exit
 fi
 
-exe_cmd "cd _fe"
-exe_cmd "npm run build-prod"
+if [ $build_fe == true ]; then
+    exe_cmd "cd _fe"
+    exe_cmd "npm run build-prod"
+fi
 exe_cmd "cd $root_dir"
 exe_cmd "jekyll build"
 if [ ! -d '_site' ];then
@@ -37,8 +44,13 @@ if [ $error_code != 0 ];then
     echo 'Switch master fail.'
     exit
 else
-    ls | grep -v _site|xargs rm -rf
-    exe_cmd "rm -rf *"
+    if [ $build_fe == true ]; then
+        ls | grep -v _site | xargs rm -rf
+    else
+        echo "keep assets"
+        ls | grep -v _site | grep -v assets | xargs rm -rf
+    fi
+    # exe_cmd "rm -rf *"
     exe_cmd "cp -r $root_dir/_site/* ."
     exe_cmd "touch .nojekyll"
 fi
